0d314dec09f7f377eb6f3e46df46317aecef0d00
li-li-github <liamiiliil@gmail.com>
2022-08-17 15:04:02 -0700

    rot3dumap_by_embryo.py only changes label color on the same layer

--- rot3dumap_by_embryo.py
+++ rot3dumap_by_embryo.py
@@ -35,46 +35,51 @@ nb_steps = 60  # number of steps between two target angles
 
 
 # Generate scatter plot on napari
-viewer = napari.Viewer()
+viewer = napari.view_points(
+    df_umap[['UMAP1', 'UMAP2', 'UMAP3']],
+    scale=(100,) * 3,
+    shading='spherical',
+    size=0.06,
+    name='umap3d',
+    edge_width=0,
+    face_color=np.zeros((1, 4)),
+    ndisplay=3,
+)
 viewer.window.resize(1000+300, 1000)
 
-# Instantiates a napari animation object for our viewer:
-animation = Animation(viewer)
 
 for tp in uniq_time:
+    print(f'Shooting the time point {tp}...')
+    # Instantiates a napari animation object for our viewer:
+    animation = Animation(viewer)
+
+    # Make a colormap
     ind = df_meta_time.get_group(tp)
     ind_gp = ind.groupby('fish')
     cmap = sns.color_palette('tab10', len(ind_gp))
+    lab_color = np.zeros((len(df_umap), 4))
     for i, (n, indf) in enumerate(ind_gp):
-        data = df_umap.loc[indf.index]
-        viewer.add_points(
-            data[['UMAP1', 'UMAP2', 'UMAP3']],
-            scale=(100,) * 3,
-            shading='spherical',
-            size=0.06,
-            name=f'{n} {tp}',
-            edge_width=0,
-            face_color=np.array(cmap[i] + (1,)).reshape(1, -1),
-        )
+        lab_color[indf.index] = np.array(cmap[i] + (1,)).reshape(1, -1)
+    viewer.layers[0].face_color = lab_color
 
-        # Ensures we are in 3D view mode:
-        viewer.dims.ndisplay = 3
-        # resets the camera view:
-        viewer.reset_view()
+    # Ensures we are in 3D view mode:
+    viewer.dims.ndisplay = 3
+    # resets the camera view:
+    viewer.reset_view()
 
-        # Start recording key frames after changing viewer state:
-        viewer.camera.angles = (0.0, 0.0, 90.0)
-        animation.capture_keyframe()
-        viewer.camera.angles = (0.0, 180.0, 90.0)
-        animation.capture_keyframe(steps=nb_steps // 2)
-        viewer.camera.angles = (0.0, 360.0, 90.0)
-        animation.capture_keyframe(steps=nb_steps // 2)
+    # Start recording key frames after changing viewer state:
+    viewer.camera.angles = (0.0, 0.0, 90.0)
+    animation.capture_keyframe()
+    viewer.camera.angles = (0.0, 180.0, 90.0)
+    animation.capture_keyframe(steps=nb_steps // 2)
+    viewer.camera.angles = (0.0, 360.0, 90.0)
+    animation.capture_keyframe(steps=nb_steps // 2)
 
-        # Render animation as a GIF:
-        animation.animate(
-            join(savepath, f'rot3DUMAP_{tp}.gif'),
-            canvas_only=True,
-            fps=60,
-            scale_factor=scale_factor
-        )
+    # Render animation as a GIF:
+    animation.animate(
+        join(savepath, f'rot3DUMAP_{tp}.gif'),
+        canvas_only=True,
+        fps=60,
+        scale_factor=scale_factor
+    )
 
